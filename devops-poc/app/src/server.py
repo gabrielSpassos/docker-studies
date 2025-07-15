ad17523dbb8f1ddf12640b52df215b18eb6c316f
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-07-15 10:04:45 -0300

    feat: add prometheus metrics to flask app

--- devops-poc/app/src/server.py
+++ devops-poc/app/src/server.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 
 from flask import Flask, request, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
 import uuid
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+metrics.info('app_info', 'Application info', version='1.0.3')
 
 @app.route('/people', methods=['POST']) 
 def create_person_api():
